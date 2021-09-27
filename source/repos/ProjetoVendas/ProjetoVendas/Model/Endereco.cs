746939ab9bb5471689cc3d43b7aeb6d9bdca2082
VictorHugoNeivaDosSantos <victorhugoneiva2017@gmail.com>
2021-09-27 12:07:37 -0400

    Adicionado contexto, Mapper e Dto. Iniciando a implementação dede repositório e serviços

---
+++ source/repos/ProjetoVendas/ProjetoVendas/Model/Endereco.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoVendas.Model
+{
+    public class Endereco
+    {
+        
+        public int Id { get; set; }
+        [JsonProperty]
+        public string Cep { get; set; }
+        public string UF { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+
+    }
+}
